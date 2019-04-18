bf7386b95e2ecc0ac385f9458c3c564ae0dc064b
강병우 <kbw2204@gmail.com>
2019-04-18 16:30:50 +0900

    Initial commit

---
+++ Video_editor/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Video_editor
+//
+//  Created by 강병우 on 18/04/2019.
+//  Copyright © 2019 강병우. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
