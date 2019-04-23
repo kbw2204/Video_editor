a70739b9607bd1910e83434a37a5de0982d08f40
강병우 <kbw2204@gmail.com>
2019-04-24 01:32:40 +0900

    add action

--- Video_editor/Controller/ViewController.swift
+++ Video_editor/Controller/ViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+@IBDesignable
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var NavigationBar: UINavigationItem!
+    @IBOutlet weak var Button_View: UIView!
+    @IBOutlet weak var Button_View2: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // 버튼뷰 제스쳐
+        let Button_View_gesture = UITapGestureRecognizer(target: self, action:  #selector(self.Button_View_Action))
+        
+        let Button_View2_gesture = UITapGestureRecognizer(target: self, action:  #selector(self.Button_View2_Action))
+        //제스쳐 등록
+        self.Button_View.addGestureRecognizer(Button_View_gesture)
+        self.Button_View2.addGestureRecognizer(Button_View2_gesture)
+        
+        
+        
+        
     }
-
-
+    @objc func Button_View_Action(sender : UITapGestureRecognizer) {
+        // Do what you want
+        print("버튼 뷰 클릭됬습니다.")
+    }
+    @objc func Button_View2_Action(sender : UITapGestureRecognizer) {
+        // Do what you want
+        print("버튼 뷰2 클릭됬습니다.")
+    }
+    @IBAction func back_Action(_ segue: UIStoryboardSegue) {
+        
+    }
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 }
-
+extension UINavigationBar {
+    open override func sizeThatFits(_ size: CGSize) -> CGSize {
+        return CGSize(width: UIScreen.main.bounds.width, height: 101)
+    }
+}
