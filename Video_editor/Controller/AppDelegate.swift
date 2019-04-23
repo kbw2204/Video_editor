b9f1088ef17ac345186a0818adef8b105eb58af6
강병우 <kbw2204@gmail.com>
2019-04-24 01:30:26 +0900

    edit navigation bar background color

--- Video_editor/Controller/AppDelegate.swift
+++ Video_editor/Controller/AppDelegate.swift
@@ -16,6 +16,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UINavigationBar.appearance().barTintColor = UIColor(red: 0.09, green: 0.10, blue: 0.10, alpha: 1.00)
+        UINavigationBar.appearance().tintColor = UIColor.white
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
+        
         return true
     }
 
