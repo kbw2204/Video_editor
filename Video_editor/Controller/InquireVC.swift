111dd6ca5111f6e15bee461ea5c48180c72b0080
강병우 <kbw2204@gmail.com>
2019-04-25 23:30:14 +0900

    Initial commit

---
+++ Video_editor/Controller/InquireVC.swift
@@ -0,0 +1,22 @@
+//
+//  InquireVC.swift
+//  Video_editor
+//
+//  Created by 강병우 on 25/04/2019.
+//  Copyright © 2019 강병우. All rights reserved.
+//
+
+import UIKit
+
+class InquireVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    // 상태바 지우기
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
