bff2f4310ecc0705d4d7eace72967e8c1927a787
강병우 <kbw2204@gmail.com>
2019-04-25 23:32:03 +0900

    세팅뷰 Segue 등록

--- Video_editor/Controller/SettingVC.swift
+++ Video_editor/Controller/SettingVC.swift
@@ -1,15 +1,15 @@
 //
-//  SettingVC.swift
+//  SettingVC_1.swift
 //  Video_editor
 //
-//  Created by 강병우 on 24/04/2019.
+//  Created by 강병우 on 25/04/2019.
 //  Copyright © 2019 강병우. All rights reserved.
 //
 
 import UIKit
 
-class SettingVC: UITableViewController {
-    let cellIndentifier = "SettingCell_id"
+class SettingVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var SettingLabel: UILabel!
     let list = [
         settingList(icon: #imageLiteral(resourceName: "shopCart"), title: "상점"),
         settingList(icon: #imageLiteral(resourceName: "star"), title: "평가 및 리뷰하기"),
@@ -17,37 +17,82 @@ class SettingVC: UITableViewController {
         settingList(icon: #imageLiteral(resourceName: "instagram"), title: "VLLO 인스타그램"),
         settingList(icon: #imageLiteral(resourceName: "facebook"), title: "VLLO 팬 페이지"),
         settingList(icon: #imageLiteral(resourceName: "message"), title: "문의하기"),
-        settingList(icon: #imageLiteral(resourceName: "cc"), title: "CC 라이센스"),
+        settingList(icon: #imageLiteral(resourceName: "Icon-eto-cc"), title: "CC 라이센스"),
     ]
     struct settingList {
         let icon : UIImage
         let title : String
     }
+    
+    let cellId = "cellId1"
+    @IBOutlet var SettingTableView: UITableView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
-        
+        self.SettingTableView.delegate = self
+        self.SettingTableView.dataSource = self
+        // Do any additional setup after loading the view.
     }
-
-    // MARK: - Table view data source
-
-    // 셀 갯수
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return list.count
-    }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "SettingCell_id") as! SettingCell
-        // 내용
-        
-        cell.SettingLabel.text = list[indexPath.row].title
-        cell.SettingImg.image = list[indexPath.row].icon
-        
-        
+    // tableview
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = SettingTableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! SettingCell
+        cell.Setting_Label.text = list[indexPath.row].title
+        cell.Setting_Image.image = list[indexPath.row].icon
         return cell
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    // 선택 된 셀
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let indexNum = indexPath.row
+        MvView(i: indexNum)
+    }
+    
+    // function
+    func MvView(i: Int) {
+        let youtube = "https://www.youtube.com/channel/UCvbmc3Bk4b_KSMxOi9-maNg"
+        let instagram = "https://www.instagram.com/vllo_official/"
+        let facebook = "https://m.facebook.com/1594140140821843"
+        let cc = "https://creativecommons.org/licenses/by/4.0/"
+        let review = "https://itunes.apple.com/kr/app/vllo-aka-vimo-video-editor/id952050883?l=en&mt=8"
+        
+        switch i {
+        case 0:         //상점
+            self.performSegue(withIdentifier: "marketSegue", sender: self)
+        case 1:
+           gotoUrl(url: review)
+        case 2:
+            gotoUrl(url: youtube)
+        case 3:
+            gotoUrl(url: instagram)
+        case 4:
+            gotoUrl(url: facebook)
+        case 5:             // 문의하기
+            self.performSegue(withIdentifier: "InquireSegue", sender: self)
+        // cc 라이센스
+        case 6:
+            gotoUrl(url: cc)
+        default:
+            print("indexNum switch err")
+        }
+    }
+    func gotoUrl(url : String) {
+        //사파리로 링크열기
+        if let url = URL(string: url) {
+            UIApplication.shared.open(url, options: [:])
+        }
+    }
+    // 뒤로가기
+    @IBAction func backToSettings(_ segue: UIStoryboardSegue) {
+    
+    }
+    // 상태바 지우기
     override var prefersStatusBarHidden: Bool {
         return true
     }
+
 }
+
--- Video_editor/Model/SettingCell.swift
+++ Video_editor/Model/SettingCell.swift
@@ -1,17 +1,27 @@
 //
-//  settingCell.swift
+//  SettingCell1.swift
 //  Video_editor
 //
-//  Created by 강병우 on 23/04/2019.
+//  Created by 강병우 on 25/04/2019.
 //  Copyright © 2019 강병우. All rights reserved.
 //
 
 import UIKit
 
 class SettingCell: UITableViewCell {
+
+    @IBOutlet var Setting_Label: UILabel!
+    @IBOutlet var Setting_Image: UIImageView!
     
-    
-    @IBOutlet var SettingImg: UIImageView!
-    @IBOutlet weak var SettingLabel: UILabel!
-    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
 }
