903da56453be48252ba5c107d06ee65344a00c78
강병우 <kbw2204@gmail.com>
2019-04-27 16:16:05 +0900

    제스쳐, gif 추가, 소스코드 정리

--- Video_editor/Controller/ViewController.swift
+++ Video_editor/Controller/ViewController.swift
@@ -13,39 +13,49 @@ class ViewController: UIViewController {
     @IBOutlet weak var NavigationBar: UINavigationItem!
     @IBOutlet weak var Button_View: UIView!
     @IBOutlet weak var Button_View2: UIView!
-    
+    @IBOutlet var gif_ImageView1: UIImageView!
+    @IBOutlet var gif_ImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // 버튼뷰 제스쳐
-        let Button_View_gesture = UITapGestureRecognizer(target: self, action:  #selector(self.Button_View_Action))
+        // tap 제스쳐 등록
+        addGesture()
         
-        let Button_View2_gesture = UITapGestureRecognizer(target: self, action:  #selector(self.Button_View2_Action))
-        //제스쳐 등록
-        self.Button_View.addGestureRecognizer(Button_View_gesture)
-        self.Button_View2.addGestureRecognizer(Button_View2_gesture)
+        // gif
+        gif_ImageView1.image = UIImage.gif(name: "vlloGif1")
+        gif_ImageView2.image = UIImage.gif(name: "vlloGif2")
+        // navigation title Gif
         
-        
-        
+        navigationItem.titleView = UIImageView(image: UIImage.gif(name: "vlloTitle"))
         
     }
-    @objc func Button_View_Action(sender : UITapGestureRecognizer) {
+    
+    // function
+    func addGesture() {
+        // 버튼뷰 제스쳐
+        let tap_Button1 = UITapGestureRecognizer(target: self, action:  #selector(self.PhtoAction(_:)))
+        let tap_Button2 = UITapGestureRecognizer(target: self, action:  #selector(self.PhtoAction(_:)))
+        let tap_gif1 = UITapGestureRecognizer(target: self, action:  #selector(self.PhtoAction(_:)))
+        let tap_gif2 = UITapGestureRecognizer(target: self, action:  #selector(self.PhtoAction(_:)))
+        
+        //제스쳐 등록
+        Button_View.addGestureRecognizer(tap_Button1)
+        Button_View2.addGestureRecognizer(tap_Button2)
+        gif_ImageView1.addGestureRecognizer(tap_gif1)
+        gif_ImageView2.addGestureRecognizer(tap_gif2)
+        
+        // 이미지뷰 터지 가능 옵션
+        gif_ImageView1.isUserInteractionEnabled = true
+        gif_ImageView2.isUserInteractionEnabled = true
+    }
+    @objc func PhtoAction(_ sender: UITapGestureRecognizer) {
         // Do what you want
         print("버튼 뷰 클릭됬습니다.")
-    }
-    @objc func Button_View2_Action(sender : UITapGestureRecognizer) {
-        // Do what you want
-        print("버튼 뷰2 클릭됬습니다.")
     }
     @IBAction func back_Action(_ segue: UIStoryboardSegue) {
         
     }
     override var prefersStatusBarHidden: Bool {
         return true
-    }
-}
-extension UINavigationBar {
-    open override func sizeThatFits(_ size: CGSize) -> CGSize {
-        return CGSize(width: UIScreen.main.bounds.width, height: 101)
     }
 }
